9377b04c6e02cfb2e5ee69d1f26eff1a0f7896f0
James Lee <aardvarkpepper@gmail.com>
2025-07-24 05:54:48 -0400

    Copied over my own code from vite-project into index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { AlertBox } from './components/AlertBox/AlertBox';
+import { UserProfileCard } from './components/UserProfileCard/UserProfileCard';
+import { ProductDisplay } from './components/ProductDisplay/ProductDisplay';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const [isClosed, setIsClosed] = useState(false);
+
+  const closeMe = () => {
+    console.log(`pre-close ${isClosed}`)
+    setIsClosed((isClosed) => !isClosed);
+    console.log(`post-close probably this is an async thing so will read the same but eh ${isClosed}`);
+  }
+
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,8 +27,26 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1>Vite + React Gerbils</h1> */}
+      <AlertBox type='success' message='hamster' onClose={() => console.log("onClose invoked.")}>
+        <div>
+          AlertBox Child 1
+        </div>
+        <div>
+          AlertBox Child 2
+        </div>
+      </AlertBox>
+      <UserProfileCard user={{id: 'UserID', name: 'User Name', email: 'user@userdomain.org', role: 'User Role', avatarUrl: 'http://userthingy.com'}} showEmail={false} showRole={true} >
+        <div>
+          User Profile Card Child 1
+        </div>
+      </UserProfileCard>
+      <ProductDisplay product={{id: 'ProductID', name: 'Product Name', price: 300, description: 'Product Description', inStock: false}} showDescription={true} showStockStatus={true}>
+        <div>
+          Product Display Card Child 1
+        </div>
+      </ProductDisplay>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -27,7 +56,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
